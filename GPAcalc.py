568a96e50b33e32405a03babee74f4885fec8342
GGatti-Coding <ggatti.guilherme@gmail.com>
2025-10-21 13:10:51 -0600

    Started the code, first few lines

---
+++ GPAcalc.py
@@ -0,0 +1,14 @@
+'''
+Assignment 1 - Programming Basics
+Group 10 - Guilherme & Jabin
+'''
+
+answer = ""
+names = []
+grades = []
+
+print("Welcome to the GPA calculator and Registry\n")
+
+while True:
+    print("Would you like to add a student? (yes or no)")
+    answer = input()
